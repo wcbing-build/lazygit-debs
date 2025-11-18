0fc420c31a46485b1e4ba1d18053d8d03fd7943c
wcbing <cbing2002@outlook.com>
2025-11-18 22:34:46 +0800

    mv check to CI; add remote trigger

--- build.sh
+++ build.sh
@@ -3,19 +3,12 @@
 PACKAGE="lazygit"
 REPO="jesseduffield/lazygit"
 
-VERSION="$(cat tag)"
+# Processing again to avoid errors of remote incoming 
+VERSION=$(echo $1 | sed -n 's|[^0-9]*\([^_]*\).*|\1|p')
 
 ARCH="amd64 arm64"
 AMD64_FILENAME="lazygit_"$VERSION"_Linux_x86_64.tar.gz"
 ARM64_FILENAME="lazygit_"$VERSION"_Linux_arm64.tar.gz"
-
-get_url_by_arch() {
-    case $1 in
-    "amd64") echo "https://github.com/$REPO/releases/latest/download/$AMD64_FILENAME" ;;
-    "arm64") echo "https://github.com/$REPO/releases/latest/download/$ARM64_FILENAME" ;;
-    esac
-}
-
 build() {
     # Prepare
     BASE_DIR="$PACKAGE"_"$VERSION"-1_"$1"
@@ -30,8 +23,17 @@ build() {
     mv "$PACKAGE-$1/$PACKAGE" "$BASE_DIR/usr/bin/$PACKAGE"
     chmod 755 "$BASE_DIR/usr/bin/$PACKAGE"
     # Build
-    dpkg-deb --build --root-owner-group -Z xz "$BASE_DIR"
+    dpkg-deb -b --root-owner-group -Z xz "$BASE_DIR" output
 }
+
+get_url_by_arch() {
+    case $1 in
+    "amd64") echo "https://github.com/$REPO/releases/latest/download/$AMD64_FILENAME" ;;
+    "arm64") echo "https://github.com/$REPO/releases/latest/download/$ARM64_FILENAME" ;;
+    esac
+}
+
+mkdir output
 
 for i in $ARCH; do
     echo "Building $i package..."
@@ -39,5 +41,6 @@ for i in $ARCH; do
 done
 
 # Create repo files
+cd output
 apt-ftparchive packages . > Packages
 apt-ftparchive release . > Release
